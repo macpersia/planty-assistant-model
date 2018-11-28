149a1369d9e571a33160b25bbcde1653a0ce9f1b
macpersia <macpersia@gmail.com>
2018-11-28 19:05:42 +0800

    Added origin.email header

--- src/main/typescript/constants.ts
+++ src/main/typescript/constants.ts
@@ -1,6 +1,7 @@
 // package models.assistant;
 
 export const PAYLOAD_TYPE_KEY = "planty.payload.type";
+export const ORIGIN_EMAIL_KEY = "planty.origin.email";
 
 // export interface Constants {
 // }
